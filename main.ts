2536ca4e7eab193f438524841e996c9e09d9d817
Austin Garcia <74737212+Augustin007@users.noreply.github.com>
2024-07-01 10:50:53 -0700

    initial commit

--- main.ts
+++ main.ts
@@ -1,89 +1,79 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Modal, Notice, Plugin, PluginSettingTab, Setting, TFile } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface DrunkDMSettings {
+	currentFile: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: DrunkDMSettings = {
+	currentFile: ''
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class DrunkDM extends Plugin {
+	settings: DrunkDMSettings;
 
 	async onload() {
+		console.log('loading plugin Drunk DM');
+
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		console.log('Settings loaded:', this.settings);
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
+		// Add a ribbon icon with a dice button
+		this.addRibbonIcon('dice', 'Roll Dice', async () => {
+			const diceInput = await this.promptForDice();
+			if (diceInput) {
+				const rollResult = this.rollDice(diceInput);
+				await this.addToCurrentFile(`Rolled ${diceInput}: ${rollResult}`);
+				new Notice(`Rolled ${diceInput}: ${rollResult}`);
 			}
 		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		// Add settings tab
+		this.addSettingTab(new DrunkDMSettingTab(this.app, this));
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		console.log('Ribbon icon and settings tab added.');
 	}
 
 	onunload() {
+		console.log('unloading plugin Drunk DM');
+	}
 
+	async promptForDice(): Promise<string | null> {
+		return new Promise((resolve) => {
+			const prompt = new DicePrompt(this.app, resolve);
+			prompt.open();
+		});
+	}
+
+	rollDice(dice: string): number {
+		console.log('Rolling dice:', dice);
+		const [num, sides] = dice.split('d').map(Number);
+		let total = 0;
+		for (let i = 0; i < num; i++) {
+		total += Math.floor(Math.random() * sides) + 1;
+		}
+		console.log('Roll result:', total);
+		return total;
+	}
+
+	async addToCurrentFile(content: string) {
+		const { vault } = this.app;
+		const filePath = `${this.settings.currentFile}.md`;
+		const file = vault.getAbstractFileByPath(filePath);
+
+		if (file instanceof TFile) {
+			const data = await vault.read(file);
+			await vault.modify(file, `${data}\n${content}`);
+			console.log('Content added to file:', filePath);
+		} else {
+			new Notice(`File ${filePath} not found`);
+			console.error('File not found:', filePath);
+		}
 	}
 
 	async loadSettings() {
 		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		console.log('Settings loaded:', this.settings);
 	}
 
 	async saveSettings() {
@@ -91,44 +81,58 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
+class DicePrompt extends Modal {
+	callback: (result: string | null) => void;
+
+	constructor(app: App, callback: (result: string | null) => void) {
 		super(app);
+		this.callback = callback;
 	}
 
 	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
+		const { contentEl } = this;
+		contentEl.createEl('h2', { text: 'Enter Dice Roll (e.g., 1d6, 2d10)' });
+		
+		const inputEl = contentEl.createEl('input', { type: 'text' });
+		inputEl.addEventListener('keypress', (event) => {
+			if (event.key === 'Enter') {
+				this.close();
+				this.callback(inputEl.value);
+			}
+		});
 	}
 
 	onClose() {
-		const {contentEl} = this;
+		const { contentEl } = this;
 		contentEl.empty();
 	}
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+class DrunkDMSettingTab extends PluginSettingTab {
+	plugin: DrunkDM;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: DrunkDM) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
+		containerEl.createEl('h2', { text: 'Drunk DM Settings' });
+
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('Current File')
+			.setDesc('The name of the file to append dice rolls to (without extension)')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setPlaceholder('Enter file name')
+				.setValue(this.plugin.settings.currentFile)
 				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.currentFile = value;
 					await this.plugin.saveSettings();
 				}));
 	}
 }
+
